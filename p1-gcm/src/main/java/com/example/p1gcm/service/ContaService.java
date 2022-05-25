b64f0b0072aaa338c6f08114542dc1830999afc2
C1326034 Luis Eduardo Rique Ferreira <c1326034@interno.bb.com.br>
2022-05-25 17:18:51 -0300

    Adicionar service de conta

--- p1-gcm/src/main/java/com/example/p1gcm/service/ContaService.java
+++ p1-gcm/src/main/java/com/example/p1gcm/service/ContaService.java
@@ -1,5 +1,64 @@
 package com.example.p1gcm.service;
 
+import com.example.p1gcm.model.Conta;
+import com.example.p1gcm.repository.ContaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ContaService {
+
+    private final ContaRepository contaRepository;
+
+    public boolean cadastrarConta(String id) {
+        contaRepository.save(new Conta(id, new BigDecimal(0)));
+        return true;
+    }
+
+    public BigDecimal consultarSaldo(String id) {
+        Optional<Conta> contaOpt = contaRepository.findById(id);
+        if(contaOpt.isPresent()) {
+            return contaOpt.get().getSaldo();
+        }
+        return null;
+    }
+
+    public boolean credito(String id, double valor) {
+        Optional<Conta> contaOpt = contaRepository.findById(id);
+        if(contaOpt.isPresent()) {
+            contaOpt.get().getSaldo().add(new BigDecimal(valor));
+            contaRepository.save(contaOpt.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean debito(String id, double valor) {
+        Optional<Conta> contaOpt = contaRepository.findById(id);
+        if(contaOpt.isPresent()) {
+            contaOpt.get().getSaldo().subtract(new BigDecimal(valor));
+            contaRepository.save(contaOpt.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferir(String idContaOrigem, String idContaDestino, double valor) {
+        Optional<Conta> contaOptOrigem = contaRepository.findById(idContaOrigem);
+        Optional<Conta> contaOptDestino = contaRepository.findById(idContaOrigem);
+        if(contaOptOrigem.isPresent() && contaOptDestino.isPresent()) {
+            this.debito(contaOptOrigem.get().getId(), valor);
+            this.credito(contaOptDestino.get().getId(), valor);
+            contaRepository.save(contaOptOrigem.get());
+            contaRepository.save(contaOptDestino.get());
+            return true;
+        }
+        return false;
+    }
+
 
 }
