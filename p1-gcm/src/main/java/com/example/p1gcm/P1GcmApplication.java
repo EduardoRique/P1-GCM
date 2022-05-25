bbe33399da83f72b255104557e0c79544b9c635c
C1326034 Luis Eduardo Rique Ferreira <c1326034@interno.bb.com.br>
2022-05-25 13:08:12 -0300

    Iniciar projeto

---
+++ p1-gcm/src/main/java/com/example/p1gcm/P1GcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.p1gcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1GcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1GcmApplication.class, args);
+	}
+
+}
---
+++ p1-gcm/src/test/java/com/example/p1gcm/P1GcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p1gcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1GcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
